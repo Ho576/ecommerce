8ec19cebc59e00b8e1647a0d2e43567368fbe74d
Ho576 <127043686+Ho576@users.noreply.github.com>
2024-05-07 19:58:30 +0300

    second task

---
+++ db/models/category.model.js
@@ -0,0 +1,30 @@
+import { Schema, Types, model } from 'mongoose';
+
+const categorySchema = new Schema({
+    name : {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    slug:{
+        type:String,
+        required: true,
+    },
+    image:{
+        type:Object,
+        required: true,
+    },
+    status:{
+        type:String,
+        default:'Active',
+        enum:['Active','NotActive'],
+    },
+   createdBy:{type:Types.ObjectId,ref:'User'},
+   updatedBy:{type:Types.ObjectId,ref:'User'},
+},
+{
+    timestamps:true,
+});
+
+const categoryModel = model('Category',categorySchema);
+export default categoryModel;
--- src/app.router.js
+++ src/app.router.js
@@ -1,16 +1,19 @@
 import connectDB from '../db/connection.js';
 import categoriesRouter from './modules/category/category.router.js';
 import productRouter from './modules/product/product.router.js';
+import authRouter from './modules/auth/auth.router.js';
 import cors from 'cors';
+
  
 const initApp = (app, express)=>{
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res)=>{
-    return res.status(200).json({message:"success"});
+    return res.status(200).json({message:"Hello in H-Shop"});
 
 })
+app.use('/auth', authRouter);
 app.use('/categories', categoriesRouter);
 app.use('/products', productRouter);
 app.use('*', (req, res)=>{
